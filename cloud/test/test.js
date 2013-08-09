8d943fc222d85504a11f6db1c61a043a3d0a33e0
Al Graham <alastair.graham@feedhenry.com>
2013-08-09 10:15:17 +0100

    adding in some tests

--- cloud/test/test.js
+++ cloud/test/test.js
@@ -1,9 +1,34 @@
-var assert = require("assert");
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+ENV_TEST = true;
+var assert = require('assert');
+var heartbeat = require('./../main').heartbeat;
+var nextPrime = require('./index').nextPrime;
+var asyncPrime = require('./index').asyncPrime;
+
+suite('nextPrime', function() {
+  test('nextPrime should return the next prime number', function() {
+    assert.equal(11, nextPrime(7));
+  });
+
+  test('zero and one are not prime numbers', function() {
+    assert.equal(2, nextPrime(0));
+    assert.equal(2, nextPrime(1));
+  });
+});
+
+suite('asyncPrime', function() {
+  test('asyncPrime should return the next prime number', function(done) {
+    asyncPrime(128, function(n) {
+      assert.equal(131, n, 'Wrong number');
+      done();
+    });
+  });
+});
+
+suite('testHeartbeat', function() {
+  test('heartbeat should return success', function(done) {
+    heartbeat({}, function(n,r) {
+      assert.equal("success", r.heartbeat, 'Success not guaranteed');
+      done();
     });
   });
 });
